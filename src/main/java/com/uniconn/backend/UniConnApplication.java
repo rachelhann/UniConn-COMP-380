3fa70c23f351aa341a46cc17dee59b9f9e6b39ad
dariatarad <108838898+dariatarad@users.noreply.github.com>
2026-02-28 16:25:26 -0800

    User Entity and Repository
    - Setup Spring Boot and application structure
    - Updated application.properties
    - Connected DB
    - Created User entity that creates users table in db
    - Created User Repository
    - Moved html and css to resources/static
    - Edited pom.xml to include all the dependencies that need to be
    installed
    - To be edited SecurityConfig

---
+++ src/main/java/com/uniconn/backend/UniConnApplication.java
@@ -0,0 +1,13 @@
+package com.uniconn.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniConnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniConnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniconn/backend/UniConnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniconn.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniConnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
